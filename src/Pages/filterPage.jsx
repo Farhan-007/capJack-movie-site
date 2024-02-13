3d88431ca3bb76c0c3716c6e60b2414c9819bbbb
Farhan akhtar <sheikhfarhan722@gmail.com>
2024-02-14 03:26:15 +0530

    tweaks

--- src/Pages/filterPage.jsx
+++ src/Pages/filterPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { NavSearchbar} from '../components/Search/search';
 import { MovieDataList } from '../features/movie/movieData';
 
--- src/features/nav/navData.js
+++ src/features/nav/navData.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 export function NavDataList() {
     const nav = useSelector((state) => state.navData.value)
     // const dispatch = useDispatch()
 
     console.log(nav)
-    
+
     return (
         <div>
             {
